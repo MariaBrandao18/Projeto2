7f6e0f59ca529652699863022bf3a08631678b75
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-05-27 09:49:04 -0300

    Update Cliente.java

--- src/classes/Cliente.java
+++ src/classes/Cliente.java
@@ -2,31 +2,55 @@ package classes;
 
 import java.util.ArrayList;
 import java.util.List;
-import classes.PacoteViagem;
-import classes.CadastroPacote;
-import conexao_db.ClienteDAO;
 
 public abstract class Cliente {
-	String nome;
-	String telefone;
-	String email;
+	private String nome;
+	private String telefone;
+	private String email;
+	private List<PacoteViagem> pacoteRelacionado;
 	
+	public Cliente() {
+		
+	}
+
 	public Cliente(String nome, String telefone, String email) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.email = email;
 		}
-	
+
 	public String getNome() {
 		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getTelefone() {
 		return telefone;
 	}
 
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<PacoteViagem> getPacoteRelacionado() {
+		return pacoteRelacionado;
+	}
+
+	public void setPacoteRelacionado(ArrayList<PacoteViagem> pacoteRelacionado) {
+		this.pacoteRelacionado = pacoteRelacionado;
+	}
+
+	
 }
 
