73660f04cc3f172b7e6df5a6455256bf12d8a4aa
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-05 10:43:51 -0300

    Update PacoteAventura.java

--- src/classes/PacoteAventura.java
+++ src/classes/PacoteAventura.java
@@ -4,9 +4,10 @@ public class PacoteAventura extends PacoteViagem {
 	
 	String tipo;
 	
-	public PacoteAventura(String nome, String destino, int duracao, double preco, String tipo, Cliente cliente) {
-		super(nome, destino, duracao, preco, cliente);
+	public PacoteAventura(Long pacote_id, String nome, String destino, int duracao, double preco, String tipo, Cliente cliente) {
+		super(pacote_id, nome, destino, duracao, preco, cliente);
 		this.tipo = "Aventura";
-		String detalhes = "Inclui trilhas e passeios de jipe";
+		//String detalhes = "Inclui trilhas e passeios de jipe";
+		pacotes.add(this);
 	}
 }
