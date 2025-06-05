1f74028060f773999f9ff533d61284dc56e7ca93
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-05 10:43:34 -0300

    Update PacoteCultural.java

--- src/classes/PacoteCultural.java
+++ src/classes/PacoteCultural.java
@@ -4,9 +4,10 @@ public class PacoteCultural extends PacoteViagem {
 	
 	String tipo;
 	
-	public PacoteCultural(String nome, String destino, int duracao, double preco, String tipo, Cliente cliente) {
-		super(nome, destino, duracao, preco, cliente);
+	public PacoteCultural(Long pacote_id, String nome, String destino, int duracao, double preco, String tipo, Cliente cliente) {
+		super(pacote_id, nome, destino, duracao, preco, cliente);
 		this.tipo = "Cultural";
-		String detalhes = "Inclui visitas a museus, centros históricos e experiências gastronômicas";
+		//String detalhes = "Inclui visitas a museus, centros históricos e experiências gastronômicas";
+		pacotes.add(this);
 	}
 }
