76ba3540f32859ec8fa0a628fed932f54ee6819d
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-07 16:15:08 -0300

    Update Cliente.java

--- src/classes/Cliente.java
+++ src/classes/Cliente.java
@@ -4,21 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Cliente {
+	private Long clienteId;
 	private String nome;
 	private String telefone;
 	private String email;
+	private String tipo_cliente;
 	private List<PacoteViagem> pacoteRelacionado;
 	
 	public Cliente() {
 		
 	}
 
-	public Cliente(String nome, String telefone, String email) {
+	public Cliente(Long clienteId, String nome, String telefone, String email) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.email = email;
 		}
 
+	
+	public Long getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(Long clienteId) {
+		this.clienteId = clienteId;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
@@ -51,6 +62,13 @@ public abstract class Cliente {
 		this.pacoteRelacionado = pacoteRelacionado;
 	}
 
+	public String getTipo_cliente() {
+		return tipo_cliente;
+	}
+
+	public void setTipo_cliente(String tipo_cliente) {
+		this.tipo_cliente = tipo_cliente;
+	}
 	
 }
 
