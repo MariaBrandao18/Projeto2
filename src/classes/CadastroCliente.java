217c2cfa7dccb974b55c69f1fc04f92b7cef5766
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-07 16:14:28 -0300

    Update CadastroCliente.java
    Completei algumas funções mas ainda falta outras, como cadastrar cliente e decidir o que fazer com clientePacote

--- src/classes/CadastroCliente.java
+++ src/classes/CadastroCliente.java
@@ -1,11 +1,15 @@
 package classes;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 import conexao_db.ClienteDAO;
 
 public class CadastroCliente {
 	static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	private static final ClienteDAO clienteDAO = new ClienteDAO();
 	
 	public CadastroCliente(){
 		
@@ -13,36 +17,95 @@ public class CadastroCliente {
 	}
 	
 	
-	public void Listar() {
-		JOptionPane.showMessageDialog(null, "Clientes Listados.");
-		for (int i = 0; i < clientes.size(); i++) {
-			JOptionPane.showMessageDialog(null, clientes.get(i).nome);
-		}
+	public static void ListarCliente() throws SQLException{
+		List<Cliente> clientes = clienteDAO.listarClientes();
+    	if(clientes == null || clientes.size() == 0) {
+    		JOptionPane.showMessageDialog(null, "Não há clientes", "Erro", JOptionPane.ERROR_MESSAGE);    		
+    		return;
+    	}
+    	
+    	StringBuilder dadosCliente = new StringBuilder();
+    	for(Cliente c: clientes) {
+    		dadosCliente.append("Nome: " + c.getNome() + "\n");
+    		dadosCliente.append("Telefone: " + c.getTelefone() + "\n");
+    		dadosCliente.append("\n----------------\n");
+    		dadosCliente.append("Pacotes: \n");
+    		List<PacoteViagem> pacotes = clienteDAO.getClientePacote(c.getClienteId());
+    		
+    		for(PacoteViagem d: pacotes) {
+    			dadosCliente.append(d.getNome() + "\n");
+    		}
+    		dadosCliente.append("\n<<<<<<<<<<<<<<<\n");
+    		
+    		dadosCliente.append("\n-----------------------\n");
+    	}
+    	JOptionPane.showMessageDialog(null, dadosCliente.toString(), "Clientes:", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
-	public void Procurar(){
-		String nome = JOptionPane.showInputDialog("Nome procurado:");
-		for (int i = 0; i < clientes.size(); i++) {
-			if (clientes.get(i).nome.equals(nome)) {
-				JOptionPane.showMessageDialog(null, "Cliente encontrado! Nome: " + clientes.get(i).getNome() + " Telefone: " + clientes.get(i).getTelefone() + " Email: " + clientes.get(i).getEmail());
-				}
-			}
+	public static void ProcurarCliente() throws SQLException{
+		String nome = JOptionPane.showInputDialog("Digite o CPF do aluno:");
+        if (nome == null || nome.trim().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nome obrigatório!", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        Cliente cliente = clienteDAO.buscarCliente(nome);
+        if (cliente == null) {
+            JOptionPane.showMessageDialog(null, "Cliente não encontrado.", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        StringBuilder mensagem = new StringBuilder();
+        mensagem.append("Dados do aluno:\n");
+        mensagem.append("Nome: ").append(cliente.getNome()).append("\n");
+        mensagem.append("Telefone: ").append(cliente.getTelefone()).append("\n");
+        //mensagem.append("CPF: ").append(cliente.getCpf()).append("\n");
+        
+        // Adicionar disciplinas
+        List<PacoteViagem> pacotes = clienteDAO.getClientePacote(cliente.getClienteId());
+        mensagem.append("Pacotes cadastrados:\n");
+        if (pacotes.isEmpty()) {
+            mensagem.append("Nenhuma disciplina matriculada.");
+        } else {
+            for (PacoteViagem p : pacotes) {
+                mensagem.append("- ").append(p.getNome()).append(" (").append(p.getPacoteId()).append(")\n");
+            }
+        }
+        
+        JOptionPane.showMessageDialog(null, mensagem.toString(), "Dados do Cliente", JOptionPane.INFORMATION_MESSAGE);
 		}
 	
-	public void Eliminar() {
-		boolean removido = false;
-		String nomeRemove = JOptionPane.showInputDialog("Nome procurado:");
-		for (int i = 0; i < clientes.size(); i++) {
-			if (clientes.get(i).nome.equals(nomeRemove)) {
-				clientes.remove(i);
-				JOptionPane.showMessageDialog(null, "Nome " + nomeRemove + " removido!");
-				removido = true;
-				break;
-			}
-		}
-		if(!removido) {
-			JOptionPane.showMessageDialog(null, "Cliente não encontrado ou já removido!");
-		}
+	public void EliminarCliente() throws SQLException{
+		String nome = JOptionPane.showInputDialog("Digite o nome do Cliente:");
+        if (nome == null || nome.trim().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nome obrigatório!", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        Cliente cliente = clienteDAO.buscarCliente(nome);
+        if (nome == null) {
+            JOptionPane.showMessageDialog(null, "Cliente não encontrado.", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        StringBuilder dadosCliente = new StringBuilder();
+        dadosCliente.append("Nome: " + cliente.getNome() + "\n");
+		dadosCliente.append("Telefone: " + cliente.getTelefone() + "\n");
+		dadosCliente.append("\n----------------\n");
+		dadosCliente.append("Pacotes: \n");
+        
+        int confirmacao = JOptionPane.showConfirmDialog(null, 
+        		dadosCliente.toString() + "\n\nTem certeza que deseja remover este cliente?", 
+                "Confirmar Remoção", 
+                JOptionPane.YES_NO_OPTION, 
+                JOptionPane.WARNING_MESSAGE);
+        
+        if (confirmacao == JOptionPane.YES_OPTION) {
+            clienteDAO.deletarCliente(cliente.getNome());
+            JOptionPane.showMessageDialog(null, "Cliente removido.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Operação cancelada.", "Cancelado", JOptionPane.INFORMATION_MESSAGE);
+        }
 	}
 	public void InserirDAO(Cliente cliente) {
 		CadastroPacote.adicionarCliente(cliente);
@@ -50,7 +113,7 @@ public class CadastroCliente {
 		conectivo.inserirCliente(cliente);
 		}
 	
-	public void clientePacote() {
+	/*public void clientePacote() {
 		String nome = JOptionPane.showInputDialog("Nome do cliente:");
 		boolean clienteEncontrado = false;
 		for(Cliente c: clientes) {
@@ -73,5 +136,5 @@ public class CadastroCliente {
 	        JOptionPane.showMessageDialog(null, "Cliente não encontrado.");
 	    }
 
-	}
+	}*/
 }
