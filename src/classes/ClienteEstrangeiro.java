2eba1c3de0e6dbc1152647cfe2ac7943932d5f37
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-07 16:16:23 -0300

    Update ClienteEstrangeiro.java
    Estendeu a variavel id

--- src/classes/ClienteEstrangeiro.java
+++ src/classes/ClienteEstrangeiro.java
@@ -3,8 +3,16 @@ package classes;
 public class ClienteEstrangeiro extends Cliente {
 	protected String passaporte;
 	
-	public ClienteEstrangeiro(String nome,String telefone, String email, String passaporte) {
-		super(nome,telefone,email);
+	public ClienteEstrangeiro(){
+		
+	}
+
+	public ClienteEstrangeiro(Long clienteId, String nome, String telefone, String email, String passaporte) {
+		super(clienteId, nome, telefone,email);
+		this.passaporte = passaporte;
+	}
+	
+	public void setPassaporte(String passaporte) {
 		this.passaporte = passaporte;
 	}
 
