db474c8204cb717b673b40655c28d8c4f0c53c60
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-10 15:55:34 -0300

    Update PacoteLuxuoso.java
    Novo construtor

--- src/classes/PacoteLuxuoso.java
+++ src/classes/PacoteLuxuoso.java
@@ -4,6 +4,16 @@ public class PacoteLuxuoso extends PacoteViagem {
 	
 	String tipo;
 	
+	public PacoteLuxuoso(Long pacote_id, String nome, String destino, int duracao, double preco) {
+		super(pacote_id, nome, destino, duracao, preco);
+	}
+	
+	public PacoteLuxuoso(Long pacote_id, String nome, String destino, int duracao, double preco, String tipo) {
+		super(pacote_id, nome, destino, duracao, preco);
+		this.tipo = "Luxuoso";
+		pacotes.add(this);
+	}
+	
 	public PacoteLuxuoso(Long pacote_id, String nome, String destino, int duracao, double preco, String tipo, Cliente cliente) {
 		super(pacote_id, nome, destino, duracao, preco, cliente);
 		this.tipo = "Luxo";
