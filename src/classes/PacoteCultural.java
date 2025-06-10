5bcc2a2b86dccef1be4f67b677ebf5a1999c31c0
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2025-06-10 15:56:08 -0300

    Update PacoteCultural.java
    Novos construtores

--- src/classes/PacoteCultural.java
+++ src/classes/PacoteCultural.java
@@ -4,6 +4,16 @@ public class PacoteCultural extends PacoteViagem {
 	
 	String tipo;
 	
+	public PacoteCultural(Long pacote_id, String nome, String destino, int duracao, double preco) {
+		super(pacote_id, nome, destino, duracao, preco);
+	}
+	
+	public PacoteCultural(Long pacote_id, String nome, String destino, int duracao, double preco, String tipo) {
+		super(pacote_id, nome, destino, duracao, preco);
+		this.tipo = "Cultural";
+		pacotes.add(this);
+	}
+	
 	public PacoteCultural(Long pacote_id, String nome, String destino, int duracao, double preco, String tipo, Cliente cliente) {
 		super(pacote_id, nome, destino, duracao, preco, cliente);
 		this.tipo = "Cultural";
